55305c254192496e0af8e7cf66e3af464efac65e
arpitaaaa23 <arpitaaaa23@gmail.com>
2026-06-10 12:55:53 +0530

    Add files via upload

---
+++ accessing elements.py
@@ -0,0 +1,12 @@
+#positive indexing
+fruits=['apple','banana','mango']
+print(fruits[0])
+print([1])
+
+#negative indexing
+fruits=['apple','banana','mango']
+print(fruits[-1])
+
+ #slicing
+fruits=['apple','banana','mango']
+print(fruits[0:2])
