1117cec1f2ad89f337c8f243520a3501947bdcb4
arpitaaaa23 <arpitaaaa23@gmail.com>
2026-06-10 12:56:14 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+fruits=['Apple','Banana','Mango']
+mixed=[10,'Hello',3.14,True]
+print(fruits)
+print(mixed)
+
+#nested list
+matrix=[[1,2],[3,4]]
+print(matrix)
+
+#list with duplicates
+nums=[1,2,2,3,3,3]
+print(nums)
