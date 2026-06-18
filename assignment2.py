917bd445e8ca101a4f2277c1412ea204af6eb1b0
arpitaaaa23 <arpitaaaa23@gmail.com>
2026-06-18 11:58:17 +0530

    day9

--- assignment2.py
+++ assignment2.py
@@ -1,9 +1,3 @@
-#Take two numbers from the user and display:Addition Subtraction Multiplication Division Reminder
-
-num1=int(input("enter num1"))
-num2=int(input("enter num2"))
-print("addition:",num1+num2)
-print("subtraction:",num1-num2)
-print("multiplication:",num1*num2)
-print("division:",num1/num2)
-print("remainder:",num1%num2)
+num=int(input("enter a number:"))
+for i in range(1,11):
+    print(num,"x",i,"=",num*i)
