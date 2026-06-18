d7432817eb269cec9064692a4a6d755b1d43ee82
arpitaaaa23 <arpitaaaa23@gmail.com>
2026-06-18 12:26:29 +0530

    day6

---
+++ for.py
@@ -0,0 +1,27 @@
+#range(start,stop,step)
+# for i in range(5):
+#     print(i)
+
+# for i in range(0,100):
+#     print(i, end=" ")
+
+# for i in range(0,100,2):
+#     print(i,end=" ")
+
+# for i in range(1,11):
+#     print(f"5*{i}={5*i}")#f for format
+
+# for i in range(5):
+#     for j in range(5):
+#         print("hello world")
+    
+# for i in range(6,0,-1):
+#     print("*" * i)
+# for i in range(6):
+#     print("*"*(5-i))
+# for j in range(2,6):
+#    print("*"*(j))
+
+rows=5
+for i in range(rows):
+    print("*" * (2 * i +1) + " " * (rows-i-1))
